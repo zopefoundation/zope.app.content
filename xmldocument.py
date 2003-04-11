da50521adeb1b510b62088f4845a524341d2cd07
Philipp von Weitershausen <philipp@weitershausen.de>
2003-04-11 14:44:27 +0000

    Refactored dynamic interface settings based on XML schemas so it can be used in other code.

--- xmldocument.py
+++ xmldocument.py
@@ -12,13 +12,12 @@
 #
 ##############################################################################
 """
-$Id: xmldocument.py,v 1.2 2003/04/10 13:04:43 faassen Exp $
+$Id: xmldocument.py,v 1.3 2003/04/11 14:44:27 philikon Exp $
 """
 from persistence import Persistent
-from zope.app.interfaces.annotation import IAttributeAnnotatable
 from zope.app.interfaces.content.xmldocument import IXMLDocument
-from zope.app.xml.w3cschemalocations import getW3CXMLSchemaLocations
-from zope.app.component.globalinterfaceservice import interfaceService
+from zope.app.xml.w3cschemalocations import\
+     setInstanceInterfacesForXMLText
 
 class XMLDocument(Persistent):
 
@@ -37,49 +36,9 @@ class XMLDocument(Persistent):
         # XML Schema interfaces are looked up for each mentioned schema,
         # and if this interface exists, we manipulate this instance to
         # state it has those interfaces.
-        
-        # XXX the interface manipulation is a hack, should be fixed after
-        # interfacegheddon
-        
-        schema_uris = getW3CXMLSchemaLocations(value)
-
-        # if there are no schemas, then we go back to whatever the class
-        # implements
-        if not schema_uris:
-            try:
-                del self.__implements__
-            except AttributeError:
-                pass
-            return
-
-        cls = self.__class__
-        if isinstance(cls.__implements__, tuple):
-            implements = list(cls.__implements__)
-        else:
-            implements = [cls.__implements__]
-
-        orig_implements = implements[:]
-        
-        for schema_uri in schema_uris:
-            interface = interfaceService.queryInterface(schema_uri, None)
-            if interface is not None and interface not in implements:
-                implements.append(interface)
-
-        # if there are no changes in the interfaces, go back to whatever
-        # the class implements
-        if implements == orig_implements:
-            try:
-                del self.__implements__
-            except AttributeError:
-                pass
-            return
-
-        self.__implements__ = tuple(implements)    
+        setInstanceInterfacesForXMLText(self)
 
     def _getSource(self):
         return self._source
     
     source = property(_getSource, _setSource)
-
-    
-            
