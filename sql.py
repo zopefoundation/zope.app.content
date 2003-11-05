e458c32f8f3af31eb9797a51543fb82f0627aac5
Jeremy Hylton <jeremy@svn.zope.org>
2003-11-05 02:46:36 +0000

    Remove more duplicate code.

--- sql.py
+++ sql.py
@@ -142,7 +142,7 @@ Inserting values with the 'sqlvar' tag
     however, if x is ommitted or an empty string, then the value
     inserted is 'null'.
 
-$Id: sql.py,v 1.10 2003/11/05 02:43:34 jeremy Exp $
+$Id: sql.py,v 1.11 2003/11/05 02:46:36 jeremy Exp $
 """
 
 import re
@@ -237,98 +237,6 @@ def parseArguments(text, result=None):
     result[name] = value
 
     return parseArguments(text[length:], result)
-
-
-class SQLVar:
-    name = 'sqlvar'
-
-    # Some defaults
-    sql_delimiter = '\0'
-
-    def sql_quote__(self, v):
-        if v.find("\'") >= 0:
-            v = "''".join(v.split("\'"))
-        return "'%s'" %v
-
-    def __init__(self, args):
-        args = parse_params(args, name='', expr='', type=None, optional=1)
-
-        name, expr = name_param(args, 'sqlvar', 1)
-        if expr is None:
-            expr = name
-        else:
-            expr = expr.eval
-        self.__name__, self.expr = name, expr
-
-        self.args = args
-        if not args.has_key('type'):
-            raise ParseError, ('the type attribute is required', 'dtvar')
-
-        t = args['type']
-        if not valid_type(t):
-            raise ParseError, ('invalid type, %s' % t, 'dtvar')
-
-
-    def render(self, md):
-        name = self.__name__
-        args = self.args
-        t = args['type']
-        try:
-            expr = self.expr
-            if isinstance(expr, StringTypes):
-                v = md[expr]
-            else:
-                v = expr(md)
-        except (KeyError, ValueError):
-            if args.has_key('optional') and args['optional']:
-                return 'null'
-            if not isinstance(expr, StringTypes):
-                raise
-            raise MissingInput, 'Missing input variable, **%s**' % name
-
-        # XXX Shrug, should these types be really hard coded? What about
-        # Dates and other types a DB supports; I think we should make this
-        # a plugin.
-        # We might be able to reuse some of the widget conversion code. (SR)
-        if t == 'int':
-            try:
-                if isinstance(v, StringTypes):
-                    int(v)
-                else:
-                    v = str(int(v))
-            except ValueError:
-                if not v and args.has_key('optional') and args['optional']:
-                    return 'null'
-                raise ValueError, (
-                    'Invalid integer value for **%s**' % name)
-
-        elif t == 'float':
-            try:
-                if isinstance(v, StringTypes):
-                    float(v)
-                else:
-                    v = str(float(v))
-            except ValueError:
-                if not v and args.has_key('optional') and args['optional']:
-                    return 'null'
-                raise ValueError, (
-                    'Invalid floating-point value for **%s**' % name)
-
-        else:
-            orig_v = v
-            v = str(v)
-            if (not v or orig_v is None) and t == 'nb':
-                if args.has_key('optional') and args['optional']:
-                    return 'null'
-                else:
-                    raise ValueError, (
-                        'Invalid empty string value for **%s**' % name)
-
-            v = self.sql_quote__(v)
-
-        return v
-
-    __call__ = render
 
 
 class SQLTest:
